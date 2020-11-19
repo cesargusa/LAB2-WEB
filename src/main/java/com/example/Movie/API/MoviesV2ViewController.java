6eee5950421866fb6c46f816e07caed8ab6903c5
cesar gutierrez <cesargusa22@gmail.com>
2020-11-19 14:13:03 +0100

    En la clase MovieV2 he añadido la propiedad description, en MoviesV2ViewController he añadido el parametro de id para que en detalle pedido me saque una pelicula en concreto, en buscadorV2 he eliminado la parte del idioma, para ello he creado un archivo changelang.txt donde he  añadido el codigo y lo implemento en cada pagina, he personalizado los botones, he eliminado parte de javascript y he añadido las peliculas con thymeleaf, he añadido que si una pelicula es de este año saque un texto de novedad que cambia con el idioma de la pagina y he añadido codigo para filtrar peliculas

--- src/main/java/com/example/Movie/API/MoviesV2ViewController.java
+++ src/main/java/com/example/Movie/API/MoviesV2ViewController.java
@@ -13,11 +13,23 @@ import org.springframework.web.servlet.ModelAndView;
 public class MoviesV2ViewController {
     @GetMapping("/V2/movies/Buscador")
     public ModelAndView GetAll(@RequestParam( name="title", required=false,defaultValue="") String title,
-    @RequestParam(name="year",required = false,defaultValue = "") String year){
-        ArrayList<MovieV2> result = MoviesV2Controller.GetAllMovies(title, year);
+    @RequestParam(name="year",required = false,defaultValue = "") String year,
+    @RequestParam(name="id",required = false,defaultValue = "") String id){
+        ArrayList<MovieV2> result = MoviesV2Controller.GetAllMovies(title, year,id);
         
         ModelAndView mv = new ModelAndView("buscadorV2");
         mv.addObject("buscadorV2", result);
+        return mv;
+    }
+
+    @GetMapping("/V2/movies/Buscador/detail")
+    public ModelAndView GetAlls(@RequestParam( name="title", required=false,defaultValue="") String title,
+    @RequestParam(name="year",required = false,defaultValue = "") String year,
+    @RequestParam(name="id",required = false,defaultValue = "") String id){
+        ArrayList<MovieV2> result = MoviesV2Controller.GetAllMovies(title, year,id);
+        
+        ModelAndView mv = new ModelAndView("movieDetail");
+        mv.addObject("movieDetail", result);
         return mv;
     }
 
--- src/main/java/com/example/Movie/Model/MovieV2.java
+++ src/main/java/com/example/Movie/Model/MovieV2.java
@@ -2,12 +2,16 @@ package com.example.Movie.Model;
 
 public class MovieV2 extends Movie{
         private String poster;
+        private String urlId;
+        private String description;
 
         public MovieV2(){}
 
-        public MovieV2(String id, String title,String year, String poster){
+        public MovieV2(String id, String title,String year, String poster,String description){
             super(id,title,year);
             this.poster=poster;
+            this.description=description;
+            urlId();
         }
 
         public String getPoster() {
@@ -18,5 +22,23 @@ public class MovieV2 extends Movie{
             this.poster = poster;
         }
 
+        public String getUrlId() {
+            return urlId;
+        }
+
+        public void setUrlId(String urlId) {
+            this.urlId = urlId;
+        }
+        public void urlId(){
+            this.urlId= "http://localhost:9092/V2/movies/Buscador/detail?id=" + getId();
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
             
 }
